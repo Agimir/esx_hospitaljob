cad97de3264da2c0df93ab54dc8d37751069d47f
Agimir <57418247+Agimir@users.noreply.github.com>
2022-05-12 13:34:15 -0500

    Update names & pay

--- esx_hospitaljob.sql
+++ esx_hospitaljob.sql
@@ -1,15 +1,15 @@
 USE `es_extended`;
 
 INSERT INTO `addon_account` (name, label, shared) VALUES
-	('society_hospital', 'San Andreas Medical Core', 1)
+	('society_hospital', 'S.A. Medical Core', 1)
 ;
 
 INSERT INTO `addon_inventory` (name, label, shared) VALUES
-	('society_hospital', 'San Andreas Medical Core', 1)
+	('society_hospital', 'S.A. Medical Core', 1)
 ;
 
 INSERT INTO `datastore` (name, label, shared) VALUES
-	('society_hospital', 'San Andreas Medical Core', 1)
+	('society_hospital', 'S.A. Medical Core', 1)
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
@@ -17,10 +17,8 @@ INSERT INTO `jobs` (name, label) VALUES
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('hospital',0,'intern','Intern',430,'{}','{}'),
-	('hospital',1,'nurse','Nurse',650,'{}','{}'),
-	('hospital',2,'doctor','Doctor',1250,'{}','{}'),
-	('hospital',3,'boss','Med. Director',1650,'{}','{}')
+	('hospital',0,'intern','Intern',25,'{}','{}'),
+	('hospital',1,'nurse','Nurse',38,'{}','{}'),
+	('hospital',2,'doctor','Doctor',46,'{}','{}'),
+	('hospital',3,'boss','Med. Director',85,'{}','{}')
 ;
-
-
